420abd4fb4300162d2d5e01e0aacfec0a2ebf27e
taehyeong <tehyoyee@gmail.com>
2023-12-25 23:17:25 +0900

    add basic-google-oauth2Login

---
+++ basic-google-oauth/src/main/java/com/example/basicgoogleoauth/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.example.basicgoogleoauth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.logging.Logger;
+
+@Controller
+public class MainController {
+
+    private Logger logger =
+            Logger.getLogger(MainController.class.getName());
+
+}
---
+++ basic-google-oauth/src/test/java/com/example/basicgoogleoauth/BasicGoogleOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basicgoogleoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicGoogleOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
